5f024726ece950a363b02af451dd34b7ef12b9b1
Chris Granstrom <45301498+CGranstrom@users.noreply.github.com>
2022-10-03 13:22:18 -0700

    run inference on directory of directories of images

--- demo.py
+++ demo.py
@@ -1,4 +1,7 @@
 import sys
+
+from utils.data_viz import display_flow
+from utils.file_io import save_flow_image
 sys.path.append('core')
 
 import argparse
@@ -16,12 +19,14 @@ from core.utils.utils import InputPadder
 import data
 import models
 import demo_frames
+import output
 
 model_dir = os.path.dirname(models.__file__)
 model_path = os.path.join(model_dir, "raft-sintel.pth")
+
 data_path = os.path.dirname(data.__file__)
-image_path = os.path.join(data_path, "frames/sintel_frames/market_2/final")
-image_path = os.path.dirname(demo_frames.__file__)
+frames_dir = os.path.join(data_path, "frames")
+output_dir = os.path.dirname(output.__file__)
 
 
 
@@ -41,12 +46,8 @@ def viz(img, flo):
     flo = flow_viz.flow_to_image(flo)
     img_flo = np.concatenate([img, flo], axis=0)
 
-    # import matplotlib.pyplot as plt
-    # plt.imshow(img_flo / 255.0)
-    # plt.show()
-
     cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
-    cv2.waitKey()
+    cv2.waitKey(100)
 
 
 def demo(args):
@@ -56,27 +57,41 @@ def demo(args):
     model = model.module
     model.to(DEVICE)
     model.eval()
+    
+    for parent_dir, sub_dirs, images in os.walk(args.data_dir):
+        if "sintel" in parent_dir or "sintel" in sub_dirs:
+            pass
+        else:
+            with torch.no_grad():
+                
+                images = tuple(os.path.join(parent_dir, image) for image in images)
+                images = sorted(images)
 
-    with torch.no_grad():
-        images = glob.glob(os.path.join(args.path, '*.png')) + \
-                 glob.glob(os.path.join(args.path, '*.jpg'))
-        
-        images = sorted(images)
-        for imfile1, imfile2 in zip(images[:-1], images[1:]):
-            image1 = load_image(imfile1)
-            image2 = load_image(imfile2)
+                for idx, (imfile1, imfile2) in enumerate(zip(images[:-1], images[1:])):
+                    image1 = load_image(imfile1)
+                    image2 = load_image(imfile2)
 
-            padder = InputPadder(image1.shape)
-            image1, image2 = padder.pad(image1, image2)
+                    padder = InputPadder(image1.shape)
+                    image1, image2 = padder.pad(image1, image2)
 
-            flow_low, flow_up = model(image1, image2, iters=3, test_mode=True)
-            viz(image1, flow_up)
-
+                    flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+                    
+                    flo = flow_up[0].permute(1,2,0).cpu().numpy()
+                    flo = flow_viz.flow_to_image(flo)
+                    flow_up = flow_up[0].permute(1,2,0).cpu().numpy()
+                    
+                    image1 = image1[0].permute(1,2,0).cpu().numpy()
+                    
+                    #display_flow(flow_up, None, image1)
+                    dir_name_for_frame_src = os.path.basename(parent_dir)
+                    output_path = os.path.join(output_dir, dir_name_for_frame_src)
+                    save_flow_image(flow_up, idx, output_path)
+            
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default=model_path, help="restore checkpoint")
-    parser.add_argument('--path', default=image_path, help="dataset for evaluation")
+    parser.add_argument('--data_dir', default=frames_dir, help="dataset for evaluation")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
